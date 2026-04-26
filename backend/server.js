7537c96cf939b00b70053f33cde4324d439eaffe
PINNINTI CHANDINI <chandinipinninti7@gmail.com>
2026-04-26 17:11:21 +0530

    Fix MongoDB connection for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const authRoutes =require("./routes/authRoutes");
-const taskRoutes =require("./routes/taskRoutes");
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 
@@ -13,12 +13,12 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
-mongoose.connect(
-  "mongodb://127.0.0.1:27017/taskmanager"
-)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
