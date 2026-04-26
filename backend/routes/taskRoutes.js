f41471aa7e9cb3d762c5651e20b27011c05fc147
PINNINTI CHANDINI <chandinipinninti7@gmail.com>
2026-04-26 19:18:46 +0530

    Fix the module path for Render deployment

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Task = require("../models/Task");
+const Task = require("../models/task.js");
 const authMiddleware = require("../middleware/authMiddleware");
 
 
